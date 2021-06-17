5c8b641b609af7d0774031dd87f6069db8506426
shige523 <amashige0709@gmail.com>
2021-06-17 22:21:00 +0900

    コマンドライン引数を受け取る

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,10 +1,14 @@
 import click
 
-
+# コマンドとして実行したい関数に@click.command
+# 名前を付けたオプションで渡すものは@click.option
+# 位置引数として渡すものは@click.argument
 @click.command()
-def cli():
+@click.option("--message", "-m", default="LGTM", show_default=True, help="画像に乗せる文字列")
+@click.argument("keyword")
+def cli(keyword, message):
     # LGMT画像生成ツール
-    lgtm()
+    lgtm(keyword, message)
     click.echo("lgtm")
 
 
