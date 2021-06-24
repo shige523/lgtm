56195e9629f305a61628b017990982d52ccba622
shige523 <amashige0709@gmail.com>
2021-06-24 16:03:26 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="lgtm",
+    version="1.0.0",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[
+        "Click",
+        "Pillow",
+        "requests",
+    ],
+    entry_points={"console_scripts": ["lgtm=lgtm.core:cli"]},
+)
